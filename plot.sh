a70d0b2328f5fb21ac684c50b2b6c51a8157ec32
Christoffer <chh@nextmedia.dk>
2015-03-12 12:05:37 -0700

    Revert "mult_rec works"
    This reverts commit ea91cf7d04315c8a2779d3c23bff3e6d30941889.

--- plot.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/gnuplot
-
-set terminal postscript eps enhanced color "Helvetica" 20
-set output 'test.png'
-
-plot "Naive.dat" title 'Naive', \
-     "Col.dat" title 'Col', \
-     "Row.dat" title 'Row'
